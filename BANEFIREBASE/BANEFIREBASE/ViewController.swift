5a7b72fc0f837fe8b07020c1800eb497c94011c7
Bane Manojlovic <bane1manojlovic@gmail.com>
2020-04-26 13:22:05 +0200

    Initial commit

---
+++ BANEFIREBASE/BANEFIREBASE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BANEFIREBASE
+//
+//  Created by Bane Manojlovic on 26/04/2020.
+//  Copyright © 2020 Bane Manojlovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
