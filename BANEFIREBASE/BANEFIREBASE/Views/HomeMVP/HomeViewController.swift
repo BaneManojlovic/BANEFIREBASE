f0ea94b4974f91262355e0f24a5da78d5c4dbcbd
Bane Manojlovic <bane1manojlovic@gmail.com>
2020-04-26 13:57:00 +0200

    Added LoginVC, SignUpVC, HomeVC

---
+++ BANEFIREBASE/BANEFIREBASE/Views/HomeMVP/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  BANEFIREBASE
+//
+//  Created by Bane Manojlovic on 26/04/2020.
+//  Copyright © 2020 Bane Manojlovic. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ BANEFIREBASE/BANEFIREBASE/Views/LoginMVP/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  BANEFIREBASE
+//
+//  Created by Bane Manojlovic on 26/04/2020.
+//  Copyright © 2020 Bane Manojlovic. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
